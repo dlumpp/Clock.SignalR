814260947103fb13a4c84b9ab216a89a2e9eb588
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2020-12-18 23:53:13 -0500

    add console client project

---
+++ Clock.SignalR.Client.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Clock.SignalR.Client.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:8080/clock")
+                .Build();
+
+            connection.On<DateTimeOffset>("ReceiveTimeUpdate", dto => Log(dto.ToString("u")));
+
+            await connection.StartAsync();
+
+            System.Console.Read();
+        }
+
+        private static void Log(string msg) => System.Console.WriteLine(msg);
+    }
+}
